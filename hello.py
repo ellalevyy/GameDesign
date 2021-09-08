75b0bf4555ce5378a47ef72ac2e66a2e8cb61ba9
ellalevyy <levye23@greenhill.org>
2021-09-08 12:04:11 -0500

    looping.py

--- hello.py
+++ hello.py
@@ -10,6 +10,14 @@ test2=89.0
 test3=91.0
 test4=87.0
 
+#modify program for one more test 
+#add a new test, and new var to add the test
+# ass a new variable for number of test
+
+test5=91.0
+sum = test1 + test2 + test3 + test4 + test5
+num=5 
+
 #declear a average veriable
 #sum +
 #SUB -
@@ -26,4 +34,4 @@ average= (test1+test2+test3+test4)/4
 #comand unsing keywords
 
 print(average)
-print("The test average is", average)
+print("The test average is " + str(average))
