d92a949543a81c02ab0f337aaa00090563fc51d1
ellalevyy <levye23@greenhill.org>
2021-09-08 13:00:04 -0500

    looping.py

---
+++ looping.py
@@ -0,0 +1,22 @@
+#Ella-Rose Levy
+#09-09-2021
+
+#We are going to learn how to ask the uder for help
+#We are going to learn how to loop
+
+star= int(input("please enter number of stars "))#allow to get vallues from the users 
+#print(" *  *  *  *")
+#loop
+#Type cast force a chance in a type of interger
+line = star
+for lncounter in range(line):4
+    for counter in range(star):
+        print("*", end=" ")
+    print()
+print("thank you")
+for lncounter in range(line):4
+    for counter in range(star):
+        print("*", end=" ")
+    print()
+print("thank you")
+star -=1 #star = star -1
