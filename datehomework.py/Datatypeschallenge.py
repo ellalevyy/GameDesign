7691944fe6b9bbb43527679405a0922570fcf7b3
ellalevyy <levye23@greenhill.org>
2021-09-14 17:05:00 -0500

    Challenge2:  Data Types

---
+++ datehomework.py/Datatypeschallenge.py
@@ -0,0 +1,31 @@
+
+#num 1 = 7
+# num 2 = 3.9
+# do // true
+
+import os
+os.system('cls')
+
+
+num1 = 30
+num2 = 2
+num3 = 4
+num4 = 20.1
+value = 'd'
+word = "hello"
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(num4))
+print(type(value))
+print(type(word))
+
+ 
+print(num1 + num3)
+print(num1 - num2)
+print(num1 * num2)
+print(num1/num2)
+
+print(num1 % num3)
+#modulus means the remainder 
