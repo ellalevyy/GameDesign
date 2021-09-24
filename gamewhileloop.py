4298e67600a00ca7d5c1f5a120a7be55a8d636b3
ellalevyy <levye23@greenhill.org>
2021-09-24 12:40:26 -0500

    guessthenum

--- gamewhileloop.py
+++ gamewhileloop.py
@@ -8,13 +8,31 @@ name=input("what is your name? ")
 print( "hi, ", name)
 answer= input("Would you like to play my game ")
 while ('y' in answer):
-    randy=random
+    randy=random.randrange(1,10)
     counter=0
     guess=input("please enter a integer")
+    print(randy)
     while(guess != randy):
-        guess=int(guess)
-        guess = randy
-        print("I was here")
+        try:
+            guess=int(guess)
+            guess = randy
+            check=True
+        except ValueError:
+            check=False
+        if(check):
+            if guess==randy:
+                print("You win!")
+                break
+            else: 
+                print("sorry try again")
+
+        else:
+            print("sorry try again")
+            counter +=1
+            
+
+    
+        
         #try and except so to convert  the guess 
         #if check
         #end loop
