5907c03a1abebe0030d51bccb1f30b745107f77a
ellalevyy <levye23@greenhill.org>
2021-10-13 14:57:57 -0500

    FinalWorldGamePJ

--- gamewhileloop.py
+++ gamewhileloop.py
@@ -1,48 +1,152 @@
 #Ella-Rose levy
 #learnng how to use while loops
 
-#place instructions of your game
-import os, random
+
+import random, os
 os.system('cls')
-name=input("what is your name? ")
-print( "hi, ", name)
-answer= input("Would you like to play my game ")
-while ('y' in answer):
-    randy=random.randrange(1,10)
-    tries_left=10
-    guess=input("please enter a integer")
-    while(tries_left >=1):
+#function to update dashes and letters
+def updateWord(word, guesses, letCount):
+    for letter in word:
+        if letter in guesses:
+            print(letter,end=" ")
+            letCount+=1
+        else:
+            print('_', end=' ')
+    return letCount
+#define function for Menu
+def Menu():
+    check = False
+    while check == False:
+        print("You are playing hangman. Please select a category you want to play.")
+        print("########################################")
+    ## put instructions right above menu
+        print("#                MENU                  #")
+        print("#                                      #")
+        print("#       1. Animals                     #")
+        print("#       2. Computer PArts              #")
+        print("#       3. Fruits                      #")
+        print("#       4. ScoreBoard                  #")
+        print("#       5. Exit                        #")
+        print("#    To play the game select 1-4       #")
+        print("#       To exit select 5               #")
+        print("########################################")
+        print()
+        sel=input("What would you like to do? ")
+       
+        #Checks to see if the number entered is a real number     
+        print(sel)
         try:
-            guess=int(guess)
+            sel=int(sel)
             check=True
         except ValueError:
             check=False
-        if(check):
-            if guess==randy:
-                print("You win!")
-                break
-            else: 
-                tries_left -=1
-                if tries_left >0:
-                    print("sorry try again. " + str(tries_left) + " tries left")
-                
-                    guess=input("Try a new integer")
-
-                else:
-                    print("you lost")
-               
-
-        else:
-            tries_left -=1
-            print( "Sorry you need to try another number. You have " +str(tries_left) + " tries left")
-          
-            guess=input("Try a new integer")
-            
-
+            print("Please enter a number")
     
-        
-        #try and except so to convert  the guess 
-        #if check
-        #end loop
-    answer=input("Do you want to play again ")
-print("thank you for playing, ", name)
+    
+    # add the Try and except before you force sel to be an int
+    print("Type one letter at a time to guess the word.")
+    return sel
+
+#Function to select the Word
+def selWord(sel):
+    if sel == 1:
+        word= random.choice(animals)
+    elif sel ==2:
+        word= random.choice(compParts)
+    else: 
+        word= random.choice(fruits)
+    return word
+    
+#Plays the game
+def Run_Through(sel, name, maxScore):
+    word= selWord(sel)
+    word = word.lower()
+    wordCount=len(word)
+    turns= wordCount+2  #depending on the lenght if the word
+    print(name, " Good Luck! you have", turns, "turns left") 
+    letCount=0 #variable to check if the user guessed the word
+    guesses=''
+    score=0
+    updateWord(word, guesses, letCount)
+
+    #Checking the letters the player has entered
+    #This loop continuing until the player guess all the letters or runs out of turns
+    while turns > 0 and letCount<wordCount:
+        print()
+        newguess=input (name + " Give me a letter ")
+        newguess= newguess.lower()
+        if newguess in word:
+            guesses += newguess
+            
+            print("you guessed one letter ")
+        else:
+            turns -=1
+            print("Sorry, you have ", turns, "turns left")
+        letCount=0
+        letCount= updateWord(word, guesses, letCount)
+    
+    #if your new score is higher it will replace the old lower score
+    score=3*wordCount+5*turns
+    if score > maxScore:
+        maxScore=score
+    print(maxScore)
+    return maxScore
+
+#lists of worlds for guessing    
+animals=["tiger", "elephant", "cat", "dog", "cow", "frog"]
+fruits=["banana", "strawberries", "Cherry", "grape", "orange", "apple"]
+compParts=["keyboard", "Monitors", "computer","trackpad", "case","Operating System"]
+
+name= input("What is your name? ")
+
+#openes save file for reading
+f=open("save.txt","r+")
+userData=[]
+#splits each line into name, highscore, and number
+for line in f.readlines():
+    splitLine=line.split("\t")
+    print(splitLine)
+    splitLine[2].replace("\n", "")
+    splitLine[2]=int(splitLine[2])
+    userData.append(splitLine)
+#deletes the file
+f.seek(0)
+f.truncate()
+
+maxScore=0 #to store highest Score
+sel=Menu()
+
+#Shows the result of selection 
+while sel==1 or sel==2 or sel==3 or sel==4:
+    if sel==1 or sel==2 or sel==3:
+        maxScore=Run_Through(sel, name, maxScore)
+        sel=Menu()
+    elif sel==4:
+        print(userData)
+    else:
+        print(maxScore)
+
+#checks to see if the current player has played before and ubdates the new highscore
+didFindUser=False
+for user in userData:
+    if user[0] == name:
+        didFindUser=True
+        if user[2] <= maxScore:
+            user[2] = maxScore
+
+#Opened txt file for writting 
+f=open("save.txt","w")
+
+#If user was not found, this write the user's name into the highscore list
+if didFindUser is False:
+    f.write (name + "\t Highest score:\t"+str(maxScore)+"\n")
+
+#prints information of past players
+for user in userData:
+    f.write(user[0])
+    f.write("\t")
+    f.write(user[1])
+    f.write("\t")
+    f.write(str(user[2]))
+    f.write("\n")
+    
--- hangman.py
+++ hangman.py
@@ -1,16 +1,30 @@
 import os, random
 os.system('cls')
 
-possible_words=["keyboard", "printer", "motherboared", "cpu", "cooling fan", "storange"]
+words=["keyboard", "printer", "motherboared", "cpu", "cooling fan", "storange"]
 name=input("what is your name? ")
+answer=input(name+ " , Do you want to play the game?") 
 print( "hi, ", name)
-word=random.choice(possible_words)
-word=word.lower()
-for letter in word:
-    print("_", end=" ") 
-guessed_letters = word
+while 'y' in answer:
+   word=random.choice(words)
+    word=word.lower()
+    print(word)
+    turns=len(word)+2
+    check = True
+    guesses= ' '
+    while(turns>0 and check):
+        for letter in word:
+            if letter in guesses:
+                print(letter, end=" ")
+                if len(guesses)==len(word):
+                    check = False
+#for letter in word:
+    #print= "_", end= " "
+   # print(fdashes) 
+   # print()
+#guessed_letters = word
 
 
-print(word[1])
+#print(word[1])
 
 
